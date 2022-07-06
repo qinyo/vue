54152d63d0e07f76bed69836a63e26cc4cede6db
qinyo <1147366905@qq.com>
2022-07-07 01:36:48 +0800

    路由

--- video/src/main.js
+++ video/src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
 createApp(App).mount('#app')
+// app组件渲染到public下的index.html 14行
+
+  // 5. 创建并挂载根实例
+const app = createApp({})
+//确保 _use_ 路由实例使
+//整个应用支持路由。
+app.use(router)
+
+app.mount('#app')
+
+// 现在，应用已经启动了！
---
+++ video/src/views/Star.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="star">star</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style>
+
+</style>
