4bd57c040a0c1a4c3dc78fa81e9e0f962af0a44c
qinyo <1147366905@qq.com>
2022-07-08 10:50:52 +0800

    完整路由项目

--- video1/src/main.js
+++ video1/src/main.js
@@ -2,11 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
 // app组件渲染到public下的index.html 14行
 
   // 5. 创建并挂载根实例
-const app = createApp({})
+const app = createApp(App)
 //确保 _use_ 路由实例使
 //整个应用支持路由。
 app.use(router)
